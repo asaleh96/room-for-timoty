e215a79d7f57fad686e9e47ae6f2e1c670680fdb
Ahmed Saleh <ahmedtarek961@gmail.com>
2022-11-03 21:08:58 +0000

    initial flat model CRUD

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,43 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    # redirect_to flat_path(@flat)
+  end
+
+
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:title, :description, :address, :rent)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+# User.destroy_all
+
+puts 'Creating 4 fake users...'
+4.times do
+  user = User.new(
+    first_name: Faker::Name.name,
+    last_name: Faker::Name.name,
+    password: 'password',
+    email: Faker::Internet.email,
+    interests: 'Cooking'
+  )
+  user.save!
+end
+
+puts 'Creating 10 fake flats...'
+10.times do
+  flat = Flat.new(
+    title:    Faker::Address.community,
+    description: "Nice apartment with #{Faker::Hipster.word} coffee beans.
+                  Located in a #{Faker::Adjective.positive} area of the city.",
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    available_from: Faker::Date.between(from: '2022-11-3', to: '2022-12-25'),
+    rent: ((300..1500).to_a).sample,
+    user: User.find(rand(1..4)))
+  flat.save!
+  puts "#{flat.title} has been created"
+end
+
+puts 'Finished!'
